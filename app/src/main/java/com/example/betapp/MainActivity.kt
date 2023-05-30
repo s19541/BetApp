b5eeac1ee9e19f6045baadb86a937d1a9ca07ea3
Marcin Wałachowski <mwalachowski99@gmail.com>
2023-05-30 14:46:39 +0200

    finished (I hope xd)

--- app/src/main/java/com/example/betapp/MainActivity.kt
+++ app/src/main/java/com/example/betapp/MainActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.betapp.databinding.ActivityMainBinding
+import com.example.betapp.game.GameAdapter
+import com.example.betapp.game.GameViewModel
 import com.example.betapp.userData.UserDataViewModel
 import com.google.firebase.auth.FirebaseAuth
 
@@ -33,17 +38,27 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        binding.testButton.setOnClickListener{
-            startActivity(Intent(this, UpcomingGamesActivity::class.java))
-        }
-
-        binding.testButton2.setOnClickListener{
+        binding.buttonUserBets.setOnClickListener{
             startActivity(Intent(this, UserBetsActivity::class.java))
         }
 
-        binding.testButton3.setOnClickListener{
+        binding.buttonUserBetsHistory.setOnClickListener{
             startActivity(Intent(this, UserBetsHistoryActivity::class.java))
         }
+
+        val gameViewModel = GameViewModel(application)
+
+        val adapter = GameAdapter()
+
+        binding.gamesRv.layoutManager = LinearLayoutManager(this)
+        binding.gamesRv.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        binding.gamesRv.adapter = adapter
+
+        gameViewModel.allGames.observe(this, androidx.lifecycle.Observer {
+            it.let{
+                adapter.setGames(it.values.toList().sortedWith(compareBy { it2 -> it2.dateTime }))
+            }
+        })
 
     }
 
